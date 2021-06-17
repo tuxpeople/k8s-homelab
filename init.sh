0e9a8a69007421b93bd48097150dd7955a9e0cf8
Thomas Deutsch <thomas@tuxpeople.org>
2021-06-17 21:21:47 +0200

    it's ansible, stupid!

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+ansible-galaxy install -r ansible/requirements.yml --ignore-errors
+
+# Create VMs
+ssh lab1 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node1"
+ssh lab2 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node2"
+ssh lab3 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node3"
+
+# Wait for VMs to boot
+sleep 30
+
+for i in k3s-node1 k3s-node2 k3s-node3; do 
+  ssh-keygen -R $i; ssh-keygen -R `dig +short $i`; ssh-keyscan -t rsa $i,`dig +short $i` >> ~/.ssh/known_hosts
+  ssh $i -l ansible "sudo yum -y install python38"
+done
+
+# Change into ansible dir
+cd ansible
+
+# Delete facts cache
+rm -rf .cache
+
+# run the playbooks
+ansible-playbook -i inventories/cluster.list plays/init.yaml
+ansible-playbook -i inventories/cluster.list plays/base.yaml
+ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yml
+ansible-playbook -i inventories/cluster.list plays/flux.yml
