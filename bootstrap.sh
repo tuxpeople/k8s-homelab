c5e9fd8aa665c4ac05c4c00882cf285fe3fecd25
Thomas Deutsch <thomas@tuxpeople.org>
2021-08-23 11:38:08 +0200

    loadbalancer changes to have checks if api works

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,6 +25,6 @@ rm -rf .cache
 # run the playbooks
 ansible-playbook -i inventories/cluster.list plays/init.yaml
 ansible-playbook -i inventories/cluster.list plays/base.yaml
-ansible-playbook -i inventories/cluster.list plays/loadbalancer.yaml
+ansible-playbook -i inventories/cluster.list plays/loadbalancer_init.yaml
 ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
 ansible-playbook -i inventories/cluster.list plays/flux.yaml
--- server/update.sh
+++ server/update.sh
@@ -2,8 +2,9 @@
 ansible-galaxy install -r requirements.yaml --ignore-errors
 
 # run the playbooks
-ansible-playbook -i inventories/cluster.list plays/init.yaml
-ansible-playbook -i inventories/cluster.list plays/base.yaml
-ansible-playbook -i inventories/cluster.list plays/loadbalancer.yaml
+# ansible-playbook -i inventories/cluster.list plays/init.yaml
+# ansible-playbook -i inventories/cluster.list plays/base.yaml
+ansible-playbook -i inventories/cluster.list plays/loadbalancer_init.yaml
 ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
 ansible-playbook -i inventories/cluster.list plays/flux.yaml
+ansible-playbook -i inventories/cluster.list plays/loadbalancer_finish.yaml
