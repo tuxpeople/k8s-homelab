adb7b000301ebf96cb28819da215ca310fbc32b5
Thomas Deutsch <thomas@tuxpeople.org>
2021-08-23 16:33:04 +0200

    ha-proxy changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,7 +25,7 @@ rm -rf .cache
 # run the playbooks
 ansible-playbook -i inventories/cluster.list plays/init.yaml
 ansible-playbook -i inventories/cluster.list plays/base.yaml
-ansible-playbook -i inventories/cluster.list plays/loadbalancer_init.yaml
+ansible-playbook -i inventories/cluster.list plays/loadbalancer_setup.yaml
 ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
 ansible-playbook -i inventories/cluster.list plays/flux.yaml
-ansible-playbook -i inventories/cluster.list plays/loadbalancer_finish.yaml
+ansible-playbook -i inventories/cluster.list plays/loadbalancer.yaml
