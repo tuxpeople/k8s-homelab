12adcac713b68c270ca943ea0654c58305b71cd6
Thomas Deutsch <thomas@tuxpeople.org>
2021-09-02 22:56:34 +0200

    fix

--- server/update.sh
+++ server/update.sh
@@ -4,7 +4,7 @@ ansible-galaxy install -r requirements.yaml --ignore-errors
 # run the playbooks
 ansible-playbook -i inventories/cluster.list plays/init.yaml
 ansible-playbook -i inventories/cluster.list plays/base.yaml
-ansible-playbook -i inventories/cluster.list plays/loadbalancer_setup.yaml
-ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
-ansible-playbook -i inventories/cluster.list plays/flux.yaml
-ansible-playbook -i inventories/cluster.list plays/loadbalancer.yaml
+# ansible-playbook -i inventories/cluster.list plays/loadbalancer_setup.yaml
+# ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
+# ansible-playbook -i inventories/cluster.list plays/flux.yaml
+# ansible-playbook -i inventories/cluster.list plays/loadbalancer.yaml
