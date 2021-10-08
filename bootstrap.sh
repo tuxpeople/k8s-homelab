dcdf9e81d3e9a6e1b9f3b09afa6c967746978ec4
Thomas Deutsch <thomas@tuxpeople.org>
2021-10-08 20:49:02 +0200

    pimp the scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,50 @@
 #!/usr/bin/env bash
+_step_counter=0
+step() {
+        _step_counter=$(( _step_counter + 1 ))
+        printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
+}
+
+step "Pull the git repo"
+git pull
+
+step "Ensure Ansible dependencies are installed"
 ansible-galaxy install -r server/requirements.yaml --ignore-errors
 
-# Create VMs
+step "Create VMs"
 ssh lab1 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node1"
 ssh lab2 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node2"
 ssh lab3 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node3"
 
-# Wait for VMs to boot
+step "Sleep 30s and allow VMs to boot"
 sleep 30
 
+step "Update SSH Host-Keys"
 for i in k3s-node1 k3s-node2 k3s-node3; do
   ssh-keygen -R $i; ssh-keygen -R `dig +short $i`; ssh-keyscan -t rsa $i,`dig +short $i` >> ~/.ssh/known_hosts
   ssh $i -l ansible "sudo yum -y install python38"
 done
 
-# prepare pre-commit
+step "Prepare pre-commit"
 brew install pre-commit
 pre-commit install
-
-# prepare kube-vip
-curl https://kube-vip.io/manifests/rbac.yaml > server/files/rbac.yaml
-export VIP=192.168.8.222
-export INTERFACE=eth0
-curl -sL kube-vip.io/k3s | vipAddress=$VIP vipInterface=$INTERFACE sh > server/files/vip.yaml
-git add server/files/vip.yaml server/files/rbac.yaml
-git commit -s -S -m "Update kube-vip deployment"
-git push
 
 # Change into ansible dir
 cd server
 
-# Delete facts cache
+step "Prepare/Update KUBE-VIP"
+curl -sL https://kube-vip.io/manifests/rbac.yaml > files/rbac.yaml
+export VIP=192.168.8.222
+export INTERFACE=eth0
+curl -sL kube-vip.io/k3s | vipAddress=$VIP vipInterface=$INTERFACE sh > files/vip.yaml
+git add files/vip.yaml files/rbac.yaml
+git commit -s -S -m "Update kube-vip deployment"
+git push
+
+step "Delete facts cache"
 rm -rf .cache
 
-# run the playbooks
+step "Run the playbooks"
 ansible-playbook -i inventories/cluster.list plays/init.yaml
 ansible-playbook -i inventories/cluster.list plays/base.yaml
 ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
--- server/update.sh
+++ server/update.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
+
+_step_counter=0
+step() {
+        _step_counter=$(( _step_counter + 1 ))
+        printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
+}
+
+step "Ensure Ansible dependencies are installed"
 ansible-galaxy install -r requirements.yaml --ignore-errors
 
-# run the playbooks
-# ansible-playbook -i inventories/cluster.list plays/init.yaml
-# ansible-playbook -i inventories/cluster.list plays/base.yaml
-# ansible-playbook -i inventories/cluster.list plays/loadbalancer_setup.yaml
+step "Prepare/Update KUBE-VIP"
+curl -sL https://kube-vip.io/manifests/rbac.yaml > files/rbac.yaml
+export VIP=192.168.8.222
+export INTERFACE=eth0
+curl -sL kube-vip.io/k3s | vipAddress=$VIP vipInterface=$INTERFACE sh > files/vip.yaml
+git add files/vip.yaml files/rbac.yaml
+git commit -s -S -m "Update kube-vip deployment"
+git push
+
+step "Run the playbooks"
+ansible-playbook -i inventories/cluster.list plays/init.yaml
+ansible-playbook -i inventories/cluster.list plays/base.yaml
 ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
-# ansible-playbook -i inventories/cluster.list plays/flux.yaml
-# ansible-playbook -i inventories/cluster.list plays/loadbalancer.yaml
+ansible-playbook -i inventories/cluster.list plays/flux.yaml
