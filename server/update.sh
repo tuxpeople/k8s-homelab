4092fc30cc4c7da70a87b005ce3f144a784c433c
Thomas Deutsch <thomas@tuxpeople.org>
2021-11-06 11:43:14 +0100

    adjust update.sh order

--- server/update.sh
+++ server/update.sh
@@ -18,15 +18,17 @@ git add files/vip.yaml files/rbac.yaml
 git commit -s -S -m "Update kube-vip deployment"
 git push
 
+step "Run the playbooks"
+ansible-playbook -i inventories/cluster.list plays/init.yaml
+ansible-playbook -i inventories/cluster.list plays/base.yaml
+ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
+ansible-playbook -i inventories/cluster.list plays/flux.yaml
+
+sleep 20
+
 step "Update cluster version in readme"
 K3S_VERSION=$(kubectl --kubeconfig=/Users/tdeutsch/iCloudDrive/Allgemein/kubectl/homelab.yaml get nodes | grep node1 | awk '{ print $5 }' | cut -d'+' -f1)
 sed -i "" "s/k3s-v[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][^.]\).*-orange/k3s-${K3S_VERSION}-orange/g" ../readme.md
 git add ../readme.md
 git commit -s -S -m "Update cluster version in readme"
 git push
-
-step "Run the playbooks"
-ansible-playbook -i inventories/cluster.list plays/init.yaml
-ansible-playbook -i inventories/cluster.list plays/base.yaml
-ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
-ansible-playbook -i inventories/cluster.list plays/flux.yaml
