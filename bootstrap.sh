7b174708711be495aa5e3970b74f58b842b10fa7
Thomas Deutsch <thomas@tuxpeople.org>
2021-11-12 21:21:58 +0100

    test ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ ansible-galaxy install -r server/requirements.yaml --ignore-errors
 step "Create VMs"
 ssh lab1 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 50 -y -u ansible k3s-node1"
 ssh lab2 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 50 -y -u ansible k3s-node2"
-ssh lab3 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 50 -y -u ansible k3s-node3"
+ssh lab3 -l root "kvm-install-vm create -t ubuntu2004 -a -c 4 -m 8192 -d 50 -y -u ansible k3s-node3"
 ssh lab4 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 50 -y -u ansible k3s-node-a"
 
 step "Sleep 30s and allow VMs to boot"
