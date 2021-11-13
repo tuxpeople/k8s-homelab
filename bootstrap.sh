92ead264b4bd60497ee1428cb97025a4d92bf339
Thomas Deutsch <thomas@tuxpeople.org>
2021-11-13 13:19:11 +0100

    various changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,13 @@ step() {
         printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
 }
 
+_pwd="$(pwd)"
+_basedir="${_pwd}/$(dirname $(which ${0}))"
+
+cd ${_basedir} && cd $(git rev-parse --show-toplevel)
+
+. server/files/settings.env
+
 step "Pull the git repo"
 git pull
 
@@ -12,24 +19,28 @@ step "Ensure Ansible dependencies are installed"
 ansible-galaxy install -r server/requirements.yaml --ignore-errors
 
 step "Create VMs"
-ssh lab1 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 50 -y -u ansible k3s-node1"
-ssh lab2 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 50 -y -u ansible k3s-node2"
-ssh lab3 -l root "kvm-install-vm create -t ubuntu2004 -a -c 4 -m 8192 -d 50 -y -u ansible k3s-node3"
-ssh lab4 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 50 -y -u ansible k3s-node-a"
+ssh lab1 -l root "kvm-install-vm create -t ${NODE_OS} -a -c ${MASTER_CPU} -m ${MASTER_MEM} -d ${OS_DISK} -y -u ansible k3s-node1"
+ssh lab2 -l root "kvm-install-vm create -t ${NODE_OS} -a -c ${MASTER_CPU} -m ${MASTER_MEM} -d ${OS_DISK} -y -u ansible k3s-node2"
+ssh lab3 -l root "kvm-install-vm create -t ${NODE_OS} -a -c ${MASTER_CPU} -m ${MASTER_MEM} -d ${OS_DISK} -y -u ansible k3s-node3"
+ssh lab4 -l root "kvm-install-vm create -t ${NODE_OS} -a -c ${MASTER_CPU} -m ${MASTER_MEM} -d ${OS_DISK} -y -u ansible k3s-node-a"
 
 step "Sleep 30s and allow VMs to boot"
 sleep 30
 
-ssh lab1 -l root "kvm-install-vm attach-disk -d 80 -t vdc k3s-node1"
-ssh lab2 -l root "kvm-install-vm attach-disk -d 80 -t vdc k3s-node2"
-ssh lab3 -l root "kvm-install-vm attach-disk -d 80 -t vdc k3s-node3"
-ssh lab4 -l root "kvm-install-vm attach-disk -d 80 -t vdc k3s-node-a"
+ssh lab1 -l root "kvm-install-vm attach-disk -d ${DATA_DISK} -t ${DATA_DRIVE} k3s-node1"
+ssh lab2 -l root "kvm-install-vm attach-disk -d ${DATA_DISK} -t ${DATA_DRIVE} k3s-node2"
+ssh lab3 -l root "kvm-install-vm attach-disk -d ${DATA_DISK} -t ${DATA_DRIVE} k3s-node3"
+ssh lab4 -l root "kvm-install-vm attach-disk -d ${DATA_DISK} -t ${DATA_DRIVE} k3s-node-a"
 
 step "Update SSH Host-Keys"
-for i in k3s-node1 k3s-node2 k3s-node-a; do
+for i in k3s-node1 k3s-node2 k3s-node3 k3s-node-a; do
   ssh-keygen -R $i; ssh-keygen -R `dig +short $i`; ssh-keyscan -t rsa $i,`dig +short $i` >> ~/.ssh/known_hosts
-  ssh $i -l ansible "sudo yum -y install python38"
 done
+
+# step "Install Python 3.8 on RHEL/CENTOS"
+# for i in k3s-node1 k3s-node2 k3s-node3 k3s-node-a; do
+#   ssh $i -l ansible "sudo yum -y install python38"
+# done
 
 step "Prepare pre-commit"
 brew install pre-commit
@@ -40,9 +51,9 @@ cd server
 
 step "Prepare/Update KUBE-VIP"
 curl -sL https://kube-vip.io/manifests/rbac.yaml > files/rbac.yaml
-export VIP=192.168.8.222
-export INTERFACE=eth0
-curl -sL kube-vip.io/k3s | vipAddress=$VIP vipInterface=$INTERFACE sh > files/vip.yaml
+# export VIP=${VIP}
+# export INTERFACE=${INTERFACE}
+curl -sL kube-vip.io/k3s | vipAddress=${VIP} vipInterface=${INTERFACE} sh > files/vip.yaml
 git add files/vip.yaml files/rbac.yaml
 git commit -s -S -m "Update kube-vip deployment"
 git push
@@ -55,3 +66,6 @@ ansible-playbook -i inventories/cluster.list plays/init.yaml
 ansible-playbook -i inventories/cluster.list plays/base.yaml
 ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
 ansible-playbook -i inventories/cluster.list plays/flux.yaml
+
+
+cd ${_pwd}
--- tools/redo.sh
+++ tools/redo.sh
@@ -11,13 +11,13 @@ _basedir="${_pwd}/$(dirname $(which ${0}))"
 cd ${_basedir} && cd $(git rev-parse --show-toplevel)
 
 step "Recreate k3s-node1"
-ssh lab1 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node1"
+ssh lab1 -l root "kvm-install-vm create -t ${NODE_OS} -a -c ${MASTER_CPU} -m ${MASTER_MEM} -d ${OS_DISK} -y -u ansible k3s-node1"
 kubectl delete nodes k3s-node1
 step "Sleep 30s and allow VM to boot"
 sleep 30
 ssh-keygen -R k3s-node1; ssh-keygen -R `dig +short k3s-node1`; ssh-keyscan -t rsa k3s-node1,`dig +short k3s-node1` >> ~/.ssh/known_hosts
-ssh k3s-node1 -l ansible "sudo yum -y install python38"
-ssh lab1 -l root "kvm-install-vm attach-disk -d 80 -t vdc k3s-node1"
+#ssh k3s-node1 -l ansible "sudo yum -y install python38"
+ssh lab1 -l root "kvm-install-vm attach-disk -d ${DATA_DISK} -t ${DATA_DRIVE} k3s-node1"
 step "Run the playbooks"
 rm -rf .cache/facts/k3s-node1
 ansible-playbook -i inventories/cluster.list plays/init.yaml
@@ -25,13 +25,13 @@ ansible-playbook -i inventories/cluster.list plays/base.yaml
 ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
 
 step "Recreate k3s-node2"
-ssh lab2 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node2"
+ssh lab2 -l root "kvm-install-vm create -t ${NODE_OS} -a -c ${MASTER_CPU} -m ${MASTER_MEM} -d ${OS_DISK} -y -u ansible k3s-node2"
 kubectl delete nodes k3s-node2
 step "Sleep 30s and allow VM to boot"
 sleep 30
 ssh-keygen -R k3s-node2; ssh-keygen -R `dig +short k3s-node2`; ssh-keyscan -t rsa k3s-node2,`dig +short k3s-node2` >> ~/.ssh/known_hosts
-ssh k3s-node2 -l ansible "sudo yum -y install python38"
-ssh lab2 -l root "kvm-install-vm attach-disk -d 80 -t vdc k3s-node2"
+#ssh k3s-node2 -l ansible "sudo yum -y install python38"
+ssh lab2 -l root "kvm-install-vm attach-disk -d ${DATA_DISK} -t ${DATA_DRIVE} k3s-node2"
 step "Run the playbooks"
 rm -rf .cache/facts/k3s-node2
 ansible-playbook -i inventories/cluster.list plays/init.yaml
@@ -39,13 +39,13 @@ ansible-playbook -i inventories/cluster.list plays/base.yaml
 ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
 
 step "Recreate k3s-node3"
-ssh lab3 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node3"
+ssh lab3 -l root "kvm-install-vm create -t ${NODE_OS} -a -c ${MASTER_CPU} -m ${MASTER_MEM} -d ${OS_DISK} -y -u ansible k3s-node3"
 kubectl delete nodes k3s-node3
 step "Sleep 30s and allow VM to boot"
 sleep 30
 ssh-keygen -R k3s-node2; ssh-keygen -R `dig +short k3s-node2`; ssh-keyscan -t rsa k3s-node2,`dig +short k3s-node2` >> ~/.ssh/known_hosts
-ssh k3s-node2 -l ansible "sudo yum -y install python38"
-ssh lab3 -l root "kvm-install-vm attach-disk -d 80 -t vdc k3s-node3"
+#ssh k3s-node2 -l ansible "sudo yum -y install python38"
+ssh lab3 -l root "kvm-install-vm attach-disk -d ${DATA_DISK} -t ${DATA_DRIVE} k3s-node3"
 step "Run the playbooks"
 rm -rf .cache/facts/k3s-node3
 ansible-playbook -i inventories/cluster.list plays/init.yaml
@@ -54,13 +54,13 @@ ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
 ansible-playbook -i inventories/cluster.list plays/k3s_cluster.yaml
 
 step "Recreate k3s-node-a"
-ssh lab4 -l root "kvm-install-vm create -a -c 4 -m 8192 -d 40 -y -u ansible k3s-node-a"
+ssh lab4 -l root "kvm-install-vm create -t ${NODE_OS} -a -c ${MASTER_CPU} -m ${MASTER_MEM} -d ${OS_DISK} -y -u ansible k3s-node-a"
 kubectl delete nodes k3s-node-a
 step "Sleep 30s and allow VM to boot"
 sleep 30
 ssh-keygen -R k3s-node2; ssh-keygen -R `dig +short k3s-node2`; ssh-keyscan -t rsa k3s-node2,`dig +short k3s-node2` >> ~/.ssh/known_hosts
-ssh k3s-node2 -l ansible "sudo yum -y install python38"
-ssh lab4 -l root "kvm-install-vm attach-disk -d 80 -t vdc k3s-node-a"
+#ssh k3s-node2 -l ansible "sudo yum -y install python38"
+ssh lab4 -l root "kvm-install-vm attach-disk -d ${DATA_DISK} -t ${DATA_DRIVE} k3s-node-a"
 step "Run the playbooks"
 rm -rf .cache/facts/k3s-node-a
 ansible-playbook -i inventories/cluster.list plays/init.yaml
