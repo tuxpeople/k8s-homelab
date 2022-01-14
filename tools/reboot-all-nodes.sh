7ed38c0d93c97d5c699b65df030365d7b824f461
Thomas Deutsch <thomas@tuxpeople.org>
2022-01-14 12:41:41 +0100

    update script

--- tools/reboot-all-nodes.sh
+++ tools/reboot-all-nodes.sh
@@ -12,6 +12,15 @@ cd ${_basedir} && cd $(git rev-parse --show-toplevel)
 
 export KUBECONFIG=~/iCloudDrive/Allgemein/kubectl/homelab.yaml
 
+step "Disable monitoring"
+MONITORS=$(curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Cache-Control: no-cache" -d "api_key=${SECRET}&format=json&logs=1" "https://api.uptimerobot.com/v2/getMonitors" | jq '.monitors[].id')
+SECRET=$(kubectl -n flux-system get secrets cluster-secrets -o go-template='{{ .data.SECRET_UPTIMEROBOT_APIKEY | base64decode }}')
+for i in $(echo ${MONITORS}); do
+  sleep 10
+  curl -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "api_key=${SECRET}&format=json&id=${i}&status=0" "https://api.uptimerobot.com/v2/editMonitor"
+  echo
+done
+
 for i in $(kubectl get nodes -o name | cut -d'/' -f2); do
   step "Update SSH Host-Key for ${i}"
   ssh-keygen -R ${i}; ssh-keygen -R `dig +short ${i}`; ssh-keyscan -t rsa ${i},`dig +short ${i}` >> ~/.ssh/known_hosts
@@ -28,6 +37,15 @@ for i in $(kubectl get nodes -o name | cut -d'/' -f2); do
 
   step "Sleep for 1 minute"
   sleep 60
+done
+
+step "Pause for 2 minutes before enabeling monitoring"
+
+step "Enable monitoring"
+for i in $(echo ${MONITORS}); do
+  curl -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "api_key=${SECRET}&format=json&id=${i}&status=1" "https://api.uptimerobot.com/v2/editMonitor"
+  echo
+  sleep 10
 done
 
 cd ${_pwd}
