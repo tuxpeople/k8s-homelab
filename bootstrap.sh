c8424b8a0415b33e568d0dcdd706572ff6f1bc21
Thomas Deutsch <thomas@tuxpeople.org>
2022-01-14 18:24:27 +0100

    this is kind of error-handling

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,5 @@ task pre-commit:init
 task ansible:deps
 task ansible:clean-cache
 task ansible:playbook:ubuntu-prepare
-task ansible:playbook:k3s-install
+task ansible:playbook:k3s-install || task ansible:playbook:k3s-install
 task ansible:playbook:flux
