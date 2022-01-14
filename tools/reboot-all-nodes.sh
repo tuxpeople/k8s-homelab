76106dd5f1f90298623e54703423a6c303c64cdb
Thomas Deutsch <thomas@tuxpeople.org>
2022-01-14 12:34:56 +0100

    update script

--- tools/reboot-all-nodes.sh
+++ tools/reboot-all-nodes.sh
@@ -10,12 +10,21 @@ _basedir="${_pwd}/$(dirname $(which ${0}))"
 
 cd ${_basedir} && cd $(git rev-parse --show-toplevel)
 
-for i in k3s-node1 k3s-node2 k3s-node3 k3s-node-a; do
+export KUBECONFIG=~/iCloudDrive/Allgemein/kubectl/homelab.yaml
+
+for i in $(kubectl get nodes -o name | cut -d'/' -f2); do
   step "Update SSH Host-Key for ${i}"
   ssh-keygen -R ${i}; ssh-keygen -R `dig +short ${i}`; ssh-keyscan -t rsa ${i},`dig +short ${i}` >> ~/.ssh/known_hosts
 
+  step "Drain ${i}"
+  kubectl drain --ignore-daemonsets --delete-emptydir-data --force ${i}
+
   step "Reboot ${i}"
-  ssh ${i} -l root "reboot"
+  ssh ${i} -l ansible "reboot"
+
+  step "Uncordon ${i}"
+  sleep 5
+  kubectl uncordon ${i}
 
   step "Sleep for 1 minute"
   sleep 60
