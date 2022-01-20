0d54e6f7d6f84323adef749a8a067b99661eac09
Thomas Deutsch <thomas@tuxpeople.org>
2022-01-21 00:11:36 +0100

    sleep longer

--- cluster/apps/utils/backup/scripts/pv-restore.sh
+++ cluster/apps/utils/backup/scripts/pv-restore.sh
@@ -36,7 +36,7 @@ wait_for_helmreleases() {
     echo "$(date +%X) - Trying to speed that up..."
     while kubectl get helmreleases.helm.toolkit.fluxcd.io -A | grep "failed\|exhausted" | grep -v STATUS; do
       kubectl get helmreleases.helm.toolkit.fluxcd.io -A | grep "failed\|exhausted" | grep -v STATUS | awk '{ print $1 " " $2 }' | xargs -L1 kubectl delete helmreleases.helm.toolkit.fluxcd.io -n
-      sleep 30
+      sleep 60
     done
     sleep 60
   done
