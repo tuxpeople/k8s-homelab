0014df9edb31e800655925580c6018e79e95b12c
Thomas Deutsch <thomas@tuxpeople.org>
2022-01-21 00:08:46 +0100

    date to step

--- cluster/apps/utils/backup/scripts/pv-restore.sh
+++ cluster/apps/utils/backup/scripts/pv-restore.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 _step_counter=0
 step() {
   _step_counter=$(( _step_counter + 1 ))
-  printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
+  printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$(date +%X) - $@" >&2  # bold cyan
 }
 
 disable_monitoring() {
