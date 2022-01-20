075c02ea64be02ce7c82771045e295a374937334
Thomas Deutsch <thomas@tuxpeople.org>
2022-01-20 21:50:16 +0100

    prepare restore

--- cluster/apps/utils/backup/scripts/pv-restore.sh
+++ cluster/apps/utils/backup/scripts/pv-restore.sh
@@ -1,2 +1,70 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+_step_counter=0
+step() {
+  _step_counter=$(( _step_counter + 1 ))
+  printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
+}
+
+disable_monitoring() {
+  step "Disable monitoring"
+  SECRET=$(kubectl -n flux-system get secrets cluster-secrets -o go-template='{{ .data.SECRET_UPTIMEROBOT_APIKEY | base64decode }}')
+  MONITORS=$(curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Cache-Control: no-cache" -d "api_key=${SECRET}&format=json&logs=1" "https://api.uptimerobot.com/v2/getMonitors" | jq '.monitors[].id')
+  for i in $(echo ${MONITORS}); do
+    sleep 10
+    curl -s -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "api_key=${SECRET}&format=json&id=${i}&status=0" "https://api.uptimerobot.com/v2/editMonitor"
+    echo
+  done
+}
+
+enable_monitoring() {
+  step "Enable monitoring"
+  sleep 30
+  SECRET=$(kubectl -n flux-system get secrets cluster-secrets -o go-template='{{ .data.SECRET_UPTIMEROBOT_APIKEY | base64decode }}')
+  MONITORS=$(curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Cache-Control: no-cache" -d "api_key=${SECRET}&format=json&logs=1" "https://api.uptimerobot.com/v2/getMonitors" | jq '.monitors[].id')
+  for i in $(echo ${MONITORS}); do
+    sleep 10
+    curl -s -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "api_key=${SECRET}&format=json&id=${i}&status=1" "https://api.uptimerobot.com/v2/editMonitor"
+    echo
+  done
+}
+
+disable_monitoring
+
+step "Get all pvcs that match the label pv-backup/enabled=true"
+source_ns_pvcs=$(kubectl get pvc --all-namespaces -l pv-backup/enabled=true -o=json | jq -c '.items[] | {name: .metadata.name, namespace: .metadata.namespace}')
+
+for ns_pvc in $source_ns_pvcs; do
+  source_pvc_name=$(jq -r '.name'<<<"$ns_pvc")
+  source_pvc_ns=$(jq -r '.namespace'<<<"$ns_pvc")
+
+  # get the associated deployment for this pvc
+  deploy_details=$(kubectl get deploy --all-namespaces -o=json | \
+    jq --arg pvc "$source_pvc_name" -c '.items[] | {name: .metadata.name, namespace: .metadata.namespace, claimName: .spec.template.spec |  select( has ("volumes") ).volumes[] | select( has ("persistentVolumeClaim") ).persistentVolumeClaim.claimName } | select(.claimName==$pvc)')
+  deploy_name=$(jq -r '.name'<<<"$deploy_details")
+  deploy_ns=$(jq -r '.namespace'<<<"$deploy_details")
+
+  step "Scale down the deployent $deploy_name down, backup pvc $source_pvc_name and scale the deployment up."
+  # scale down the deployment so we can mount the PVC later
+  kubectl scale -n "$deploy_ns" deploy "$deploy_name" --replicas=0
+
+  # wait for the deployment to successfully scale down
+  # shellcheck disable=SC2046
+  while [ $(kubectl get po -l app.kubernetes.io/name="$deploy_name" -n "$deploy_ns" -ojson | jq -r '.items | length') -ne 0 ]; do
+    sleep 1
+  done
+
+  sleep 5
+
+  # copy from source pvc to dest pvc
+  kubectl -n rook-ceph exec $(kubectl -n rook-ceph get pod -l "app=rook-direct-mount" -o jsonpath='{.items[0].metadata.name}') -- /scripts/restore.sh --rbd $(kubectl get pv/$(kubectl get pv | grep "$source_pvc_name" | awk -F' ' '{print $1}') -n "${deploy_ns}" -o json | jq -rj '.spec.csi.volumeAttributes.imageName') --pvc "$source_pvc_name"
+  # scale deployment back up; blindly do not wait so we can move on to the next backup
+  # TODO: maybe wait in background?
+
+  sleep 5
+
+  kubectl scale -n "$deploy_ns" deploy "$deploy_name" --replicas=1
+done
+
+trap enable_monitoring EXIT
---
+++ cluster/core/rook-ceph/rook-direct-mount/restore.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# PVC=sonarr-config-v1 \
+# NS=media \
+# kubectl -n rook-ceph exec -it $(kubectl -n rook-ceph get pod -l "app=rook-direct-mount" -o jsonpath='{.items[0].metadata.name}') -- /scripts/backup.sh --rbd $(kubectl get pv/$(kubectl get pv | grep "$PVC" | awk -F' ' '{print $1}') -n "${NS}" -o json | jq -rj '.spec.csi.volumeAttributes.imageName') --pvc "$PVC"
+
+# Set defaults
+NFS_MOUNTPATH="/mnt/nas-data"
+RBD_MOUNTPATH="/mnt/data"
+CURRENT_DATE=$(date +"%FT%H%M")
+rbd=""
+pvc=""
+
+# Collect command line parameters
+while [ $# -gt 0 ]; do
+    if [[ "$1" == *"--"* ]]; then
+        param="${1/--/}"
+        declare "$param"="$2"
+    fi
+    shift
+done
+
+if [[ -z "${rbd}" ]]; then
+    echo "Required parameter '--rbd' not set!"
+    exit 1
+fi
+
+if [[ -z "${pvc}" ]]; then
+    echo "Required parameter '--pvc' not set!"
+    exit 1
+fi
+
+if ! mountpoint -q ${NFS_MOUNTPATH}; then
+    echo "NFS mount '${NFS_MOUNTPATH}' is not mounted"
+    exit 1
+fi
+
+LATEST_BACKUP=$(ls -1t ${NFS_MOUNTPATH}/Backups/ | grep ${pvc} | head -1)
+
+# ceph osd pool ls
+rbd map -p ceph-blockpool "${rbd}" | xargs -I{} mount {} "${RBD_MOUNTPATH}"
+rm -rf ${RBD_MOUNTPATH}/*
+tar xzf "${LATEST_BACKUP}" -C "${RBD_MOUNTPATH}/" .
+umount "${RBD_MOUNTPATH}"
+rbd unmap -p ceph-blockpool "${rbd}"
