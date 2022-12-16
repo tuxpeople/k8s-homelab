efda04c4515d1aaadc39c541784300f632baa296
Thomas Deutsch <thomas@tuxpeople.org>
2022-12-16 22:56:16 +0100

    change script

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,13 +23,8 @@ task cluster:reconcile
 
 echo "$(date +%X) - Check for helmreleases"
 while kubectl get helmreleases.helm.toolkit.fluxcd.io -A | grep -v STATUS | grep -v succeeded > /dev/null; do
-echo "$(date +%X) - Wait until all helmreleases are ready"
-echo "$(date +%X) - Trying to speed that up..."
-while kubectl get helmreleases.helm.toolkit.fluxcd.io -A | grep "failed\|exhausted" | grep -v STATUS; do
-    kubectl get helmreleases.helm.toolkit.fluxcd.io -A | grep "failed\|exhausted" | grep -v STATUS | awk '{ print $1 " " $2 }' | xargs -L1 kubectl delete helmreleases.helm.toolkit.fluxcd.io -n
-    sleep 60
-done
-task cluster:reconcile
+task cluster:hr-restart
+sleep 180
 done
 
 # kubectl logs -n utils -l app=pv-restore-job -f
