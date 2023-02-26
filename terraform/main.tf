8145a541316415d73b0652fe26b7c6ddba762fd2
Thomas Deutsch <thomas@tuxpeople.org>
2023-02-26 17:57:34 +0100

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,26 @@
 terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.34.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.2.1"
+    }
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "2.3.1"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "0.7.2"
+    }
+  }
   backend "local" {
   }
 }
 
-module "vsphere" {
-    source = "./vsphere"
-}
-
-module "cloudflare" {
-    source = "./cloudflare"
+data "sops_file" "terraform_secrets" {
+  source_file = "secret.sops.yaml"
 }
