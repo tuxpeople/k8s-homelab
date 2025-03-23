cf9f2259dbc498e72699011169fa313e1c80d3ce
Thomas Deutsch <thomas@tuxpeople.org>
2025-03-23 11:05:59 +0100

    change script

--- kubernetes/apps/productivity/paperless/app/scripts/remove-blank-pages.sh
+++ kubernetes/apps/productivity/paperless/app/scripts/remove-blank-pages.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
+#set -x -e -o pipefail
 set -e -o pipefail
 export LC_ALL=C
 
 #IN="$1"
 IN="$DOCUMENT_WORKING_PATH"
 
-PAGES=$(pdfinfo "$IN" | grep ^Pages: | tr -dc '0-9')
-THRESHOLD=0.002
+# Check for PDF format
+TYPE=$(file -b "$IN")
+
+if [ "${TYPE%%,*}" != "PDF document" ]; then
+    echo >&2 "Skipping $IN - non PDF [$TYPE]."
+    exit 0
+fi
+
+# PDF file - proceed
+
+#PAGES=$(pdfinfo "$IN" | grep ^Pages: | tr -dc '0-9')
+PAGES=$(pdfinfo "$IN" | awk '/Pages:/ {print $2}')
+
+echo >&2 Total pages $PAGES
+
+# Threshold for HP scanners
+# THRESHOLD=1
+# Threshold for Canon MX925
+THRESHOLD=1
 
 non_blank() {
     for i in $(seq 1 $PAGES); do
-        PERCENT=$(gs -o - -dFirstPage=${i} -dLastPage=${i} -sDEVICE=inkcov "${IN}" | grep CMYK | nawk 'BEGIN { sum=0; } {sum += $1 + $2 + $3 + $4;} END { printf "%.5f\n", sum } ')
+        PERCENT=$(gs -o - -dFirstPage=${i} -dLastPage=${i} -sDEVICE=ink_cov "${IN}" | grep CMYK | nawk 'BEGIN { sum=0; } {sum += $1 + $2 + $3 + $4;} END {  printf "%.5f\n", sum } ')
+        echo >&2 -n "Color-sum in page $i is $PERCENT: "
         if awk "BEGIN { exit !($PERCENT > $THRESHOLD) }"; then
             echo $i
+            echo >&2 "Page added to document"
         else
-            echo >&2 Color-sum is $PERCENT: will remove blank page $i of $IN
+            echo >&2 "Page removed from document"
         fi
     done
 }
