b3ab1c6d9a8b5af1457e905208a4842452f44a73
Thomas Deutsch <thomas@tuxpeople.org>
2025-06-21 13:31:48 +0200

    housekeeping

--- scripts/ensure-yaml-separator.sh
+++ scripts/ensure-yaml-separator.sh
@@ -6,7 +6,7 @@ TARGET_DIR="kubernetes"
 find "$TARGET_DIR" -type f -name '*.yaml' | while read -r file; do
     first_line=$(head -n 1 "$file" || true)
     if [[ "$first_line" != "---"* ]]; then
-        sed -i '1i ---' "$file"
+        gsed -i '1i ---' "$file"
         echo "Added '---' to $file"
     fi
 done
