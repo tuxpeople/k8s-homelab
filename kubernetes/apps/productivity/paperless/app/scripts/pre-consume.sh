27b64a2a41cc754c7d748d68199b08947e93a07a
Thomas Deutsch <thomas@tuxpeople.org>
2025-09-16 22:12:01 +0200

    trying to work around stuff

--- kubernetes/apps/productivity/paperless/app/scripts/pre-consume.sh
+++ kubernetes/apps/productivity/paperless/app/scripts/pre-consume.sh
@@ -8,24 +8,6 @@ set -e
 # Optionales Logging
 echo "Pre-consume script starting…"
 
-# https://github.com/ocrmypdf/OCRmyPDF/issues/1483
-cat <<"EOF"
-______ _                        _ _          _  __________________
-|  ___(_)                      (_) |        | | | ___ \  _  \  ___|
-| |_   ___  __  _ __ ___   __ _ _| | ___  __| | | |_/ / | | | |_ ___
-|  _| | \ \/ / | '_ ` _ \ / _` | | |/ _ \/ _` | |  __/| | | |  _/ __|
-| |   | |>  <  | | | | | | (_| | | |  __/ (_| | | |   | |/ /| | \__ \
-\_|   |_/_/\_\ |_| |_| |_|\__,_|_|_|\___|\__,_| \_|   |___/ \_| |___/
-EOF
-MIME_TYPE=$(file --mime-type -b "${DOCUMENT_SOURCE_PATH}")
-
-if [ "$MIME_TYPE" == "application/pdf" ]; then
-        gs -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -sOutputFile="${DOCUMENT_WORKING_PATH}" "${DOCUMENT_SOURCE_PATH}"
-fi
-
-exit 0
-
-
 # Stelle sicher, dass pymupdf installiert ist (nur beim ersten Lauf nötig, ggf. rausnehmen)
 pip install --quiet pymupdf
 
