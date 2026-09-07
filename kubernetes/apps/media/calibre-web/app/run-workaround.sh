6ef52f479876e0e1f2a7f242e3e421d4124b0b10
Thomas Deutsch <thomas@tuxpeople.org>
2026-09-07 14:32:56 +0200

    test workaround

---
+++ kubernetes/apps/media/calibre-web/app/run-workaround.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bash
+#
+# See https://github.com/crocodilestick/Calibre-Web-Automated/issues/1224
+
+set +e
+
+curl -s https://raw.githubusercontent.com/crocodilestick/Calibre-Web-Automated/refs/heads/main/root/etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run -o /etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run.original
+chmod +x /etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run.original
+/etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run.original
+setup_exit=$?
+
+if [ -x /usr/bin/calibredb ]; then
+    echo "[calibre-binaries-setup] Calibre installation verified successfully"
+    exit 0
+fi
+
+exit $setup_exit
