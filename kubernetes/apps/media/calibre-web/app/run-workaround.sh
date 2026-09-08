f97d646f07845f31a9600e69024525631470b040
Thomas Deutsch <thomas@tuxpeople.org>
2026-09-08 11:04:11 +0200

    test workaround

--- kubernetes/apps/media/calibre-web/app/run-workaround.sh
+++ kubernetes/apps/media/calibre-web/app/run-workaround.sh
@@ -3,7 +3,7 @@
 # See https://github.com/crocodilestick/Calibre-Web-Automated/issues/1224
 
 set +e
-
+echo "WORKAROUND - WORKAROUND - WORKAROUND - WORKAROUND - WORKAROUND"
 curl -s https://raw.githubusercontent.com/crocodilestick/Calibre-Web-Automated/refs/heads/main/root/etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run -o /etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run.original
 chmod +x /etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run.original
 /etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run.original
