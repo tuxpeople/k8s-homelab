f791bc8e14b4b2401a6debcd53304a1c88bd5fde
Thomas Deutsch <thomas@tuxpeople.org>
2026-09-11 10:41:40 +0200

    test workaround

--- kubernetes/apps/media/calibre-web/app/run-workaround.sh
+++ kubernetes/apps/media/calibre-web/app/run-workaround.sh
@@ -2,13 +2,13 @@
 # See https://github.com/crocodilestick/Calibre-Web-Automated/issues/1224
 
 set +e
-echo "WORKAROUND - WORKAROUND - WORKAROUND - WORKAROUND - WORKAROUND"
+echo "[calibre-binaries-setup-WORKAROUND] Starting workaround wrapper script"
 
 # Download original script we replaced
 curl -sL https://raw.githubusercontent.com/crocodilestick/Calibre-Web-Automated/refs/heads/main/root/etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run -o /etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run.original
 chmod +x /etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run.original
 
-# Create the fake xdg-desktop-menu command directly in system space
+# Create a fake xdg-desktop-menu command in system space
 echo -e '#!/bin/sh\nexit 0' > /usr/local/bin/xdg-desktop-menu
 chmod +x /usr/local/bin/xdg-desktop-menu
 
