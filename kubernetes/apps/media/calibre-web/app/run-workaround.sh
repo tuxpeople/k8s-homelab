4bc9c3628e1e7c02cac1a0247d4fd129fa7de326
Thomas Deutsch <thomas@tuxpeople.org>
2026-09-11 10:00:27 +0200

    test workaround

--- kubernetes/apps/media/calibre-web/app/run-workaround.sh
+++ kubernetes/apps/media/calibre-web/app/run-workaround.sh
@@ -10,8 +10,8 @@ chmod +x /etc/s6-overlay/s6-rc.d/calibre-binaries-setup/run.original
 setup_exit=$?
 
 if [ -x /usr/bin/calibredb ]; then
-    echo "[calibre-binaries-setup-WORKAROUND] Calibre installation verified successfully"
-    exit 0
+  echo "[calibre-binaries-setup-WORKAROUND] Calibre installation verified successfully"
+else
+  echo "[calibre-binaries-setup-WORKAROUND] Calibre installation verified successfully"
+  exit $setup_exit
 fi
-
-exit $setup_exit
